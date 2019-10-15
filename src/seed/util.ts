80145424950d03d13b2f10bc163bd20f0f6d4e88
Walter Steur <stewartrule@gmail.com>
2019-10-15 20:19:47 +0200

    Add seed utilities

---
+++ src/seed/util.ts
@@ -0,0 +1,47 @@
+type Maybe<T> = T | undefined;
+
+export const range = (min: number, max: number) =>
+  Array.from({ length: max - min }).map((_, index) => index + min);
+
+export const mapSeq = async <T, R>(
+  items: T[],
+  map: (item: T) => Promise<R>,
+): Promise<R[]> => {
+  const initial = items.slice(0, 1);
+  const rest = items.slice(1);
+
+  const ret: R[] = initial.length
+    ? await mapParallel(initial, map)
+    : [];
+
+  return rest.length ? ret.concat(await mapSeq(rest, map)) : ret;
+};
+
+export const mapParallel = <T, R>(
+  items: T[],
+  map: (item: T, index: number) => Promise<R>,
+) => Promise.all(items.map((item, i) => map(item, i)));
+
+export const getAge = (dob: Date) => {
+  const now = new Date();
+  const monthDiff = now.getMonth() - dob.getMonth();
+  const age = now.getFullYear() - dob.getFullYear();
+
+  return monthDiff < 0 ||
+    (monthDiff === 0 && now.getDate() < dob.getDate())
+    ? age - 1
+    : age;
+};
+
+export const clamp = (value: number, min: number, max: number) =>
+  Math.min(Math.max(value, min), max);
+
+export const chance = (value = 0.5) => Math.random() < value;
+
+export const random = <T>(values: T[]): Maybe<T> => {
+  const { length } = values;
+
+  return length
+    ? values[Math.floor(Math.random() * length)]
+    : undefined;
+};
