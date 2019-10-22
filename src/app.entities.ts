60c040370db2a48bd8a8e8b1341fc1fc4dcad140
Walter Steur <stewartrule@gmail.com>
2019-10-22 21:51:01 +0200

    Move app imports into seperate files

---
+++ src/app.entities.ts
@@ -0,0 +1,19 @@
+import { Game } from './game/game.entity';
+import { Intercept } from './intercept/intercept.entity';
+import { PassType } from './pass-type/pass-type.entity';
+import { Pass } from './pass/pass.entity';
+import { Player } from './player/player.entity';
+import { ShotType } from './shot-type/shot-type.entity';
+import { Shot } from './shot/shot.entity';
+import { Team } from './team/team.entity';
+
+export default [
+  Game,
+  Intercept,
+  Pass,
+  PassType,
+  Player,
+  Shot,
+  ShotType,
+  Team,
+];
