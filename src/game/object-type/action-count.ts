344ff2a05bdaf0c54be298c1a195fcc1ed716e6d
Walter Steur <stewartrule@gmail.com>
2019-10-22 21:55:52 +0200

    Add object types for game resolver

---
+++ src/game/object-type/action-count.ts
@@ -0,0 +1,22 @@
+import {
+  Field,
+  ObjectType,
+} from 'type-graphql';
+
+@ObjectType()
+export class ActionCount {
+  @Field()
+  x!: number;
+
+  @Field()
+  y!: number;
+
+  @Field()
+  count!: number;
+
+  @Field()
+  time!: Date;
+
+  @Field()
+  teamId!: number;
+}
---
+++ src/game/object-type/team-shot-count-by-period.ts
@@ -0,0 +1,18 @@
+import { Field, Int, ObjectType } from 'type-graphql';
+
+import { Shot } from '../../shot/shot.entity';
+
+@ObjectType()
+export class TeamShotCountByPeriod {
+  @Field(type => Date)
+  start!: Date;
+
+  @Field(type => Date)
+  stop!: Date;
+
+  @Field(type => Int)
+  count!: number;
+
+  @Field(type => [Shot])
+  shots!: Shot[];
+}
