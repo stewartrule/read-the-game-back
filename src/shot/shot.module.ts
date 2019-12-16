2031cce5248480886fcfe61a224793cb6e114c35
Walter Steur <stewartrule@gmail.com>
2019-12-16 11:07:50 +0100

    Add shot service

--- src/shot/shot.module.ts
+++ src/shot/shot.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Shot } from './shot.entity';
+import { ShotService } from './shot.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Shot])],
+  providers: [ShotService],
   exports: [TypeOrmModule],
 })
 export class ShotModule {}
---
+++ src/shot/shot.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Shot } from './shot.entity';
+
+type CreateShot = {
+  gameId: number;
+  playerId: number;
+  teamId: number;
+  shotTypeId: number;
+  hit: boolean;
+};
+
+@Injectable()
+export class ShotService {
+  constructor(
+    @InjectRepository(Shot)
+    private readonly shotRepository: Repository<Shot>,
+  ) {}
+
+  async create({
+    gameId,
+    playerId,
+    teamId,
+    shotTypeId,
+    hit
+  }: CreateShot): Promise<Shot> {
+    const shot = new Shot();
+
+    shot.gameId = gameId;
+    shot.playerId = playerId;
+    shot.teamId = teamId;
+    shot.typeId = shotTypeId;
+
+    shot.time = new Date();
+
+    shot.onTarget = hit;
+    shot.hit = hit;
+    shot.out = hit ? false : Math.random() > 0.5;
+    shot.x = Math.floor(Math.random() * 120);
+    shot.y = Math.floor(Math.random() * 90);
+
+    await this.shotRepository.save(shot);
+    return shot;
+  }
+}
